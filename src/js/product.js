cf8d24bb7c88c36d4844d34716141c73cdc5012a
Braden Huttash <hut21013@byui.edu>
2023-05-03 15:53:04 -0600

    team3 build and lint, fixed errors

--- src/js/product.js
+++ src/js/product.js
@@ -1,4 +1,4 @@
-import { setLocalStorage, getLocalStorage, getParams, loadHeaderFooter } from "./utils.mjs";
+import { getParams, loadHeaderFooter } from "./utils.mjs";
 
 import ProductDetails from "./ProductDetails.mjs";
 import ProductData from "./ProductData.mjs";
@@ -7,12 +7,12 @@ loadHeaderFooter();
 const dataSource = new ProductData("tents");
 const productId = getParams("product");
 
-const product = new ProductDetails(productId, dataSource);
-product.init();
+const productDetails = new ProductDetails(productId, dataSource);
+productDetails.init();
 // add to cart button event handler
 async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
+  let product = await dataSource.findProductById(e.target.dataset.id);
+  productDetails.addToCart(product);
 }
 
 // add listener to Add to Cart button
